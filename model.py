ab14c6ef14bc0ed943207b2fe73d288ea3450019
Harsha Manjunath <hmanjunath@january.Compute.TechFak.DE>
2021-08-31 22:12:07 +0200

    Branch for gru

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 class RNN(nn.Module):
     def __init__(self, d_in, d_out, n_layers=1, bi=True, dropout=0.2):
         super(RNN, self).__init__()
-        self.rnn = nn.LSTM(input_size=d_in, hidden_size=d_out, bidirectional=bi, num_layers=n_layers, dropout=dropout)
+        self.rnn = nn.GRU(input_size=d_in, hidden_size=d_out, bidirectional=bi, num_layers=n_layers, dropout=dropout)
 
     def forward(self, x, x_len):
         x_packed = pack_padded_sequence(x, x_len.cpu(), batch_first=True, enforce_sorted=False)
